18c5e3cd6b425a6528f0c31f7346c4894a2db9d4
Ing. Nelson G.E <nescalan@gmail.com>
2020-11-27 15:23:18 -0600

    13: Dibujando en el DOM

---
+++ clases/12-dom/script.js
@@ -0,0 +1,6 @@
+let ruoteWindows = window.location;
+alert("En todas, esto si funciona");
+document.write("Hola estudiante, estas en esta ruta: " + ruoteWindows);
+console.log(ruoteWindows.href);
+console.log(console);
+console.table(ruoteWindows);
