50fcadaf6d8db34a7eafe7c5486d0bd40c887e3b
Ing. Nelson G.E <nescalan@gmail.com>
2020-12-02 16:07:12 -0600

    18: Dibujar en canvas con las flechas del teclado

--- Lecciones/17-Eventos del Teclado/eventos.js
+++ Lecciones/17-Eventos del Teclado/eventos.js
@@ -1,5 +1,27 @@
-document.addEventListener("keydown", dibujarTeclado);
+var teclas = {
+  UP: 38,
+  DOWN: 40,
+  LEFT: 37,
+  RIGHT: 39,
+};
 
-function dibujarTeclado() {
-  console.log("Tecla oprimida");
+document.addEventListener("keyup", dibujarTeclado);
+
+function dibujarTeclado(evento) {
+  switch (evento.keyCode) {
+    case teclas.UP:
+      console.log("Hoy es Up.");
+      break;
+    case teclas.DOWN:
+      console.log("Hoy es Down.");
+      break;
+    case teclas.LEFT:
+      console.log("Hoy es Left. <----");
+      break;
+    case teclas.RIGHT:
+      console.log("Hoy es Right.--->");
+      break;
+    default:
+      console.log("Opcion no permitida");
+  }
 }
